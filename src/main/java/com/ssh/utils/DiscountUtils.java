3fbe9469b76cc22b244e4c6c46a49b88596a91c3
lixiang12138 <zhao20130673@163.com>
2018-12-29 00:27:15 +0800

    要 来 了

--- src/main/java/com/ssh/utils/DiscountUtils.java
+++ src/main/java/com/ssh/utils/DiscountUtils.java
@@ -12,17 +12,30 @@ public class DiscountUtils {
     {
         String ans = "<fieldset>";
         ans = ans + "优惠券类型：";
-        String rule = d.getDiscountRule();
-        if(rule.equals("shop"))ans = ans + "商店专用优惠券";
-        else if(rule.equals("class"))ans = ans + "种类专用优惠券";
-        else ans = ans + "商品专用优惠券";
+        ans = ans + "商店专用优惠券";
         ans = ans + "<br>";
         if(d.getDiscountLeast()==0)
             ans = ans + "无门槛优惠券：" + d.getDiscountPrice().toString() + "&nbsp元<br>";
         else
             ans = ans + "满&nbsp" + d.getDiscountLeast().toString() +
                     "&nbsp减&nbsp" + d.getDiscountPrice().toString() + "&nbsp元<br>";
-        ans = ans + "<></fieldset>";
+        return ans;
+    }
+    public static String shop_discount_simple(Discount d,List<Product>list)
+    {
+        String ans = "<fieldset>";
+        ans = ans + "优惠券类型：";
+        ans = ans + "商品专用优惠券：<br>";
+        Iterator<Product>it = list.iterator();
+        while(it.hasNext())
+        {
+            ans = ans + it.next().getProductName() + "&nbsp&nbsp&nbsp&nbsp单价：" + it.next().getProductPrice().toString() + "<br>";
+        }
+        if(d.getDiscountLeast()==0)
+            ans = ans + "无门槛优惠券：" + d.getDiscountPrice().toString() + "&nbsp元<br>";
+        else
+            ans = ans + "满&nbsp" + d.getDiscountLeast().toString() +
+                    "&nbsp减&nbsp" + d.getDiscountPrice().toString() + "&nbsp元<br>";
         return ans;
     }
     public static String mydiscount_haveList(List<Object[]>list)
--- src/main/java/com/ssh/utils/ShopUtils.java
+++ src/main/java/com/ssh/utils/ShopUtils.java
@@ -8,16 +8,67 @@ import java.util.Iterator;
 import java.util.List;
 
 public class ShopUtils {
-    public static String shop_discount(List<Discount>list)
+    public static String shop_discount_shop(List<Discount>list)
     {
-        String ans = "<fieldset>";
-        ans = ans + "<a href=\"editdiscount?id=\">添加新优惠券</a><br>";
+        String ans = "<fieldset><legend>商店优惠券</legend>";
+        ans = ans + "<a href=\"editdiscount?id=-1\">添加新商店优惠券</a><br>";
         Iterator<Discount>it = list.iterator();
         while(it.hasNext())
         {
             ans = ans + DiscountUtils.shop_discount_simple(it.next());
         }
         ans = ans + "</fieldset>";
+        return ans;
+    }
+    public static String shop_discount_set_product(List<Object[]>list)
+    {
+        String ans = "<fieldset><legend>商品优惠券</legend>";
+        ans = ans + "<a href=\"editdiscount?id=-2\">添加新商品优惠券</a><br>";
+        Iterator<Object[]>it = list.iterator();
+        while(it.hasNext())
+        {
+            Object[] tmp = it.next();
+            ans = ans + DiscountUtils.shop_discount_simple((Discount) tmp[0],(List<Product>)tmp[1]);
+        }
+        ans = ans + "</fieldset>";
+        return ans;
+    }
+    public static String shop_one_discount(Discount d)
+    {
+        String ans = "<fieldset>";
+        if(d.getDiscountPrice()!=null)
+        {
+            ans = ans + "满减上限：" + "<input type=\"text\" name=\"discountPrice\" value=\"" + d.getDiscountPrice().toString() +"\">";
+        }
+        else
+        {
+            ans = ans + "满减上限：" + "<input type=\"text\" name=\"discountPrice\">";
+        }
+        ans = ans + "<input type=\"hidden\" name=\"discountType\" value=\"" + d.getDiscountType() +"\"><br>" ;
+        if(d.getDiscountLeast()!=null)
+        {
+            ans = ans + "满减上限：" + "<input type=\"text\" name=\"discountLeast\" value=\"" + d.getDiscountLeast().toString() +"\">";
+        }
+        else
+        {
+            ans = ans + "满减上限：" + "<input type=\"text\" name=\"discountLeast\">";
+        }
+        ans = ans + "<input type=\"hidden\" name=\"discountRule\" value=\"" + d.getDiscountRule() +"\"><br>" ;
+        if(d.getDiscountRule().equals("shop"))
+            ans = ans + "<br><input type = \"submit\" value = \"提交\"></fieldset>";
+        return ans;
+    }
+    public static String shop_discount_product(List<Product>list)
+    {
+        String ans = "商品类别：";
+        Iterator<Product> it = list.iterator();
+        while(it.hasNext())
+        {
+            Product tmp = it.next();
+            ans = ans + "<input type=\"checkbox\" name=\"productId\"  value=\""
+                    + tmp.getProductId() + "\">" + tmp.getProductName() + "</input>";
+        }
+        ans = ans + "<br><input type = \"submit\" value = \"提交\"></fieldset>";
         return ans;
     }
     public static String shop_product(List<Object[]>list)
@@ -42,12 +93,12 @@ public class ShopUtils {
             Clazz tmp = it.next();
             ans = ans + "<option value=\"" + tmp.getClassId() + "\">" + tmp.getClassName() + "</option>";
         }
-        ans = ans + "</select><br><input type = \"submit\" value = \"提交\">";
+        ans = ans + "</select><br><input type = \"submit\" value = \"提交\"></fieldset>";
         return ans;
     }
     public static String shop_one_product(Product p)
     {
-        String ans = "";
+        String ans = "<fieldset>";
         if (p.getProductName() != null)
             ans = ans + "商品名称：" + "<input type=\"text\" name=\"productName\" value=\"" + p.getProductName() +"\">" ;
         else{
